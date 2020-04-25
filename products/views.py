477193e38135902a1380995f3b2dd4e6a6f6e05d
Hervera <nkuliherve@gmail.com>
2020-04-25 13:08:19 +0200

    feature(urls): create basic routes
    - create home route and new product route
    - create views for routes

--- products/views.py
+++ products/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return HttpResponse('Hello World')
+
+def create(request):
+    return HttpResponse('New Product')
